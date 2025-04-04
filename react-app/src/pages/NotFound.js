6d0c390006027f4bb49954268b8c5fa318522c95
Awsmin <103874344+HowDoYouWriteACommentLineAgain@users.noreply.github.com>
2025-04-04 18:05:28 +0800

    notFound page commit

---
+++ react-app/src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NotFound(){
+
+    return(
+        <>
+            <h1>404 - Page Not Found</h1>
+            <big>
+               :'(                                                 
+            </big>
+        </>
+    )
+}

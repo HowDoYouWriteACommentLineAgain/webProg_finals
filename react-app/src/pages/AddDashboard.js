cb2782ea75b47d76438a640f78ef3edf21f0d019
Awsmin <103874344+HowDoYouWriteACommentLineAgain@users.noreply.github.com>
2025-04-04 15:19:09 +0800

    removed nurses

--- react-app/src/pages/AddDashboard.js
+++ react-app/src/pages/AddDashboard.js
@@ -6,10 +6,9 @@ export default function EditDashboard(){
     const [name, setName] = useState('');
     const [spec, setSpec] = useState('');
     const [days, setDays] = useState('');
-    const [nurs, setNurs] = useState('');
     const [avai, setAvai] = useState('');
 
-    
+
 
     return(
         <>
@@ -18,7 +17,6 @@ export default function EditDashboard(){
                 <Inputs value={name} name="name" placeholder='Insert Name' onChange={(e)=>setName(e.target.value)} />
                 <Inputs value={spec} name="spec" placeholder='Insert Specializations' onChange={(e)=>setSpec(e.target.value)} />
                 <Inputs value={days} name="days" placeholder='Insert Days Available' onChange={(e)=>setDays(e.target.value)} />
-                <Inputs value={nurs} name="nurs" placeholder='Insert Nurses assigned' onChange={(e)=>setNurs(e.target.value)} />
                 <Inputs value={avai} name="avai" placeholder='Insert Availability' onChange={(e)=>setAvai(e.target.value)} />
                 <button className='btn btn-primary'>Submit</button>
             </fieldset>

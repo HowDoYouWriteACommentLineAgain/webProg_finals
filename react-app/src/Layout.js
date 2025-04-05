94ed0838fe45c21faf63607bde09f3ef74428234
Awsmin <103874344+HowDoYouWriteACommentLineAgain@users.noreply.github.com>
2025-04-05 14:25:24 +0800

    added icons

--- react-app/src/Layout.js
+++ react-app/src/Layout.js
@@ -11,11 +11,10 @@ const Layout = ({loginStatus, user}) => {
                 {/* <span className="nav-item  col-5">Connected East Avenue Medical Center | status: {loginStatus || 'Please login'}</span> */}
 
                 <span className="navbar-nav d-flex" id="collapseNavItems">
-                    {!loginStatus && <div className="nav-item"><Link className="nav-link text-white" to="/login">Login</Link></div>}
-                    {loginStatus && <div className="nav-item"><Link className="nav-link text-white" to="/logout">Logout</Link></div>}
-                    
-                    <div className="nav-item"><Link className="nav-link text-white" to="/dashboard">Dashboard</Link></div>
-                    {loginStatus && <Link className="nav-item nav-link text-white" to="/addDashboard">Create</Link>}
+                    {/*login*/!loginStatus && <div className="nav-item"><Link className="nav-link text-white" to="/login">                  <i class="bi bi-file-person fs-2"></i></Link></div>}
+                    {/*logout*/loginStatus && <div className="nav-item"><Link className="nav-link mx-2 text-white" to="/logout"><button className='btn btn-danger fs-6 mt-2'>logout</button></Link></div>}
+                    {/*RUD*/                } <div className="nav-item"><Link className="nav-link text-white" to="/dashboard">              <i class="bi bi-file-ruled fs-2 ms-3"></i></Link></div>
+                    {/*Create*/loginStatus && <div className="nav-item"><Link className="nav-item nav-link text-white" to="/addDashboard">  <i class="bi bi-file-earmark-plus fs-2 ms-3"></i></Link></div>}
                     {/* <Link className="nav-item nav-link text-white" to="/usersCrud">Config Users</Link> */}
                     
                 </span>
